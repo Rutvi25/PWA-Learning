d8dda4303fca1d0144c59b94ebe21fcbf0874c8e
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-09-05 12:11:54 +0530

    app install banner

--- PWA-udemy/public/src/js/app.js
+++ PWA-udemy/public/src/js/app.js
@@ -1,5 +1,12 @@
+// var deferredPrompt;
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(function () {
     console.log('service worker registered!!!');
   });
 }
+window.addEventListener('beforeinstallprompt', function (event) {
+  console.log('beforeinstalledprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
