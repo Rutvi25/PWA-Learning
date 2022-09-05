62a3c97fc569275612e7ca1e118956458fc3d4f9
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-09-05 15:52:01 +0530

    rejecting promise

--- PWA-udemy/public/src/js/app.js
+++ PWA-udemy/public/src/js/app.js
@@ -13,13 +13,26 @@ window.addEventListener('beforeinstallprompt', function (event) {
 
 var promise = new Promise(function (resolve, reject) {
   setTimeout(function () {
-    resolve('This is executed once the timer is done!!!');
+    // resolve('This is executed once the timer is done!!!');
+    reject({ code: 500, message: 'An error occured!' });
   }, 3000);
 });
-promise.then(function(text) {
-  return text 
-  console.log(text);
-}).then(function(newText) {
-  console.log(newText)
-});
+// promise
+//   .then(function(text) {
+//     return text;
+//     console.log(text);
+//   })
+//   .then(function (newText) {
+//     console.log(newText);
+//   });
+promise
+  .then(function (text) {
+    return text;
+  })
+  .then(function (newText) {
+    console.log(newText);
+  })
+  .catch(function (err) {
+    console.log(err.code, err.message);
+  });
 console.log('this is executed right after setTimeOut!!!');
