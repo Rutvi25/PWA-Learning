b6b7978f7d9fb8281fc896423afaf16ed9219379
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-09-06 17:57:49 +0530

    polyfills

--- PWA-udemy/public/src/js/app.js
+++ PWA-udemy/public/src/js/app.js
@@ -1,4 +1,7 @@
-// var deferredPrompt;
+var deferredPrompt;
+if(!window.Promise) {
+  window.Promise = Promise;
+}
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(function () {
     console.log('service worker registered!!!');
