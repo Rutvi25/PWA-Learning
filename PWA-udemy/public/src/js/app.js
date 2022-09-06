4af82c463465d02309379384d5f119fdad9c8e01
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-09-06 10:57:45 +0530

    fetch

--- PWA-udemy/public/src/js/app.js
+++ PWA-udemy/public/src/js/app.js
@@ -10,13 +10,45 @@ window.addEventListener('beforeinstallprompt', function (event) {
   deferredPrompt = event;
   return false;
 });
-
+// promises
 var promise = new Promise(function (resolve, reject) {
   setTimeout(function () {
     // resolve('This is executed once the timer is done!!!');
     reject({ code: 500, message: 'An error occured!' });
   }, 3000);
 });
+// fetch
+fetch('https://httpbin.org/ip')
+  .then(function (response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+fetch('https://httpbin.org/post', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  body: JSON.stringify({message: 'Does this work?'})
+})
+.then(function (response) {
+  console.log(response);
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+})
+.catch(function (err) {
+  console.log(err);
+});
+
 // promise
 //   .then(function(text) {
 //     return text;
@@ -25,6 +57,7 @@ var promise = new Promise(function (resolve, reject) {
 //   .then(function (newText) {
 //     console.log(newText);
 //   });
+
 promise
   .then(function (text) {
     return text;
