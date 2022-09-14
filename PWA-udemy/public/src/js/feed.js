010603f1076dc24ce48bc8205eaf7ebe1685bf08
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-09-14 11:15:13 +0530

    unregistring the service worker and deleting cache

--- PWA-udemy/public/src/js/feed.js
+++ PWA-udemy/public/src/js/feed.js
@@ -9,18 +9,22 @@ function openCreatePostModal() {
   createPostArea.style.display = 'block';
   if (deferredPrompt) {
     deferredPrompt.prompt();
-
     deferredPrompt.userChoice.then(function (choiceResult) {
       console.log(choiceResult.outcome);
-
       if (choiceResult.outcome === 'dismissed') {
         console.log('User cancelled installation');
       } else {
         console.log('User added to home screen');
       }
     });
-
     deferredPrompt = null;
+  }
+  if('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+      for (var i=0; i<registrations.length; i++) {
+        registrations[i].unregister()
+      }
+    })
   }
 }
 
