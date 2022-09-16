298f7cbf8ea2a11f090d61bbb5903974607b94e6
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-09-16 15:20:20 +0530

    clear database

--- PWA-udemy/public/src/js/utility.js
+++ PWA-udemy/public/src/js/utility.js
@@ -5,20 +5,27 @@ var dbPromise = idb.open('posts-store', 1, function (db) {
 });
 
 function writeData(st, data) {
-  return dbPromise
-    .then(function (db) {
-      var tx = db.transaction(st, 'readwrite');
-      var store = tx.objectStore(st);
-      store.put(data);
-      return tx.complete;
+  return dbPromise.then(function (db) {
+    var tx = db.transaction(st, 'readwrite');
+    var store = tx.objectStore(st);
+    store.put(data);
+    return tx.complete;
   });
 }
 
 function readAllData(st) {
-  return dbPromise
-    .then(function (db) {
-      var tx = db.transaction(st, 'readonly');
-      var store = tx.objectStore(st);
-      return store.getAll();
+  return dbPromise.then(function (db) {
+    var tx = db.transaction(st, 'readonly');
+    var store = tx.objectStore(st);
+    return store.getAll();
+  });
+}
+
+function clearAllData(st) {
+  return dbPromise.then(function (db) {
+    var tx = db.transaction(st, 'readwrite');
+    var store = tx.objectStore(st);
+    store.clear();
+    return tx.complete;
   });
 }
--- PWA-udemy/public/sw.js
+++ PWA-udemy/public/sw.js
@@ -1,7 +1,7 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v18';
+var CACHE_STATIC_NAME = 'static-v19';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 var STATIC_FILES = [
   '/',
@@ -81,11 +81,15 @@ self.addEventListener('fetch', function (event) {
     event.respondWith(
       fetch(event.request).then(function (res) {
         var clonedRes = res.clone();
-        clonedRes.json().then(function (data) {
-          for (var key in data) {
-            writeData('posts', data[key]);
-          }
-        });
+        clearAllData('posts')
+          .then(function () { 
+            return clonedRes.json();
+          })
+          .then(function (data) {
+            for (var key in data) {
+              writeData('posts', data[key]);
+            }
+          });
         return res;
       })
     );
