473c47d086af5ff138ecdf02ffc6fcffe47d4bac
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-09-30 14:29:38 +0530

    added event listener for capture button

--- PWA-udemy/public/src/js/feed.js
+++ PWA-udemy/public/src/js/feed.js
@@ -29,15 +29,32 @@ function initializeMedia() {
       });
     };
   }
-  navigator.mediaDevices.getUserMedia({video: true})
-    .then(function(stream) {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
       videoPlayer.srcObject = stream;
       videoPlayer.style.display = 'block';
     })
-    .catch(function(err) {
+    .catch(function (err) {
       imagePickerArea.style.display = 'block';
-    })
+    });
 }
+captureButton.addEventListener('click', function (event) {
+  canvasElement.style.display = 'block';
+  videoPlayer.style.display = 'none';
+  captureButton.style.display = 'none';
+  var context = canvasElement.getContext('2d');
+  context.drawImage(
+    videoPlayer,
+    0,
+    0,
+    canvas.width,
+    videoPlayer.videoHeight / (videoPlayer.videoWidth / canvas.width)
+  );
+  videoPlayer.srcObject.getVideoTracks().forEach(function (track) {
+    track.stop();
+  });
+});
 
 function openCreatePostModal() {
   // createPostArea.style.display = 'block';
