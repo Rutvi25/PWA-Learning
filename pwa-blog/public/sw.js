f2f1209949bfdcb28f04e5c3193f7f15d29012e5
Rutvi Patel <rutvipatel2500@gmail.com>
2022-10-24 16:03:04 +0530

    fetching data

--- pwa-blog/public/sw.js
+++ pwa-blog/public/sw.js
@@ -4,9 +4,22 @@ this.addEventListener('install', (event) => {
   event.waitUntill(
     caches.open(cacheData).then((cache) => {
       cache.addAll([
-        // '/static/js/bundle.js',
-
+        '/static/js/bundle.js',
+        '/static/js/main.chunk.js',
+        '/static/js/0.chunk.js',
+        '/index.html',
+        '/'
       ]);
     })
   );
 });
+
+this.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((result) => {
+      if(result) {
+        return result
+      }
+    })
+  )
+})
--- pwa-blog/src/Users.js
+++ pwa-blog/src/Users.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
 
 function Users() {
-  return <div>Users</div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    let url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url).then((response) => {
+      response.json().then((result) => {
+        console.log("result", result);
+        setData(result);
+      });
+    });
+  }, []);
+  return (
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.address.street}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default Users;
